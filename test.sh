db003cdedc7922e51f9215584db40bd79a3662e6
Omar-Gamaleldin <omargamal01000001@gmail.com>
2026-05-01 16:45:49 +0200

    testing vllm server

---
+++ test.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+# Here you can add bash commands:
+#SBATCH --container-mounts=/etc/slurm/task_prolog:/etc/slurm/task_prolog,/scratch:/scratch,/usr/lib64/slurm:/usr/lib64/slurm,/usr/lib64/libhwloc.so:/usr/lib64/libhwloc.so,/usr/lib64/libhwloc.so.15:/usr/lib64/libhwloc.so.15,/pfs/work9/workspace/scratch/ul_ekd37-gepa-optimization/:/pfs/work9/workspace/scratch/ul_ekd37-gepa-optimization
+#SBATCH --container-mount-home
+#SBATCH --output=slurm_log/gepa/%x_%j.out  # Output file for job logs %x for job-name and %j for job-id
+#SBATCH --container-name=vllm_qwen3.5
+#SBATCH --job-name=Qwen3.5_9B
+#SBATCH --container-workdir $HOME/VLMs-Medical-Imaging/
+#SBATCH --gres=gpu:1
+
+vllm serve models/Qwen3.5-9B
+	--port 8001
+	--mm-encoder-tp-mode data \
+	--mm-processor-cache-type shm \
+	--reasoning-parser qwen3 \
+	--enable-prefix-caching
+
